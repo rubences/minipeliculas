e4ecac55793057358d3e6f5df2819d2c9df8ad02
rubences <91731294+rubences@users.noreply.github.com>
2025-03-10 11:19:27 +0000

    cambios

--- Pelicula.py
+++ Pelicula.py
@@ -3,7 +3,7 @@ class Pelicula:
         self.titulo = titulo
         self.duracion = duracion
         self.anio = anio
-        print(f"Se ha creado la película: {self.titulo}")
+
 
     def __str__(self):
-        return f"{self.titulo} ({self.anio})"
+        return f"Se ha creado la película: {self.titulo} ({self.anio})"
